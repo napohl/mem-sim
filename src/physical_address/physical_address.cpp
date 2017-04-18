e69ab1408153a5e64fe7f0997b5f1d1d68cf61f1
npohl <npohl@mymail.mines.edu>
2017-04-18 17:18:16 -0600

    Got the to string funciton in physcial adress working.

--- src/physical_address/physical_address.cpp
+++ src/physical_address/physical_address.cpp
@@ -6,14 +6,19 @@
 
 #include "physical_address/physical_address.h"
 #include <sstream>
+#include <bitset>
 
 using namespace std;
 
 
 string PhysicalAddress::to_string() const {
-    stringstream stream;
-    stream << frame << offset;
-    return stream.str();
+    string address;
+    //frame is 10 bits, so size is 10 bits
+    bitset<10> frameBit(frame);
+    //offset is 6 bits, so size is 6 bits
+    bitset<6> offsetBit(offset);
+    address = frameBit.to_string() + offsetBit.to_string();
+    return address;
 }
 
 
