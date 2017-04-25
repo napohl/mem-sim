d4fa5a2177b5d76aabdc88d5ac0c4b6ce8ca848a
npohl <npohl@mymail.mines.edu>
2017-04-24 19:20:37 -0600

    Frame tests now passing.

--- src/frame/frame.cpp
+++ src/frame/frame.cpp
@@ -10,5 +10,12 @@ using namespace std;
 
 
 void Frame::set_page(Process* process, size_t page_number) {
-    //implement me
+    if (process->is_valid_page(page_number)) {
+        contents = process->pages[page_number];
+    }
+    else {
+        contents = nullptr;
+    }
+    Frame::process = process;
+    Frame::page_number = page_number;
 }
