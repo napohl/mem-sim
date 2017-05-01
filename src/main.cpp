2f3f4e5ad2dac0ab3e4ee2c87241cddac976f7d3
npohl <npohl@mymail.mines.edu>
2017-05-01 11:21:00 -0600

    Had to fix sim.txt so that it would not cause seg faults with the example binary given to me. Main will now print final results, although we need to figure out our simulation. I think that we are going to have to pass most stuff to the simulation::run() function.

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,26 @@
 using namespace std;
 
 /**
+ * helper function for printing out results of the simulation
+ */
+void printEndResults(vector<int> pids, vector<Process*> processes, int totalMemAccess, int totalPageFaults, int totalUsedFrames) {
+    cout << "\nDONE!\n\n";
+    //pids and processes should be the same size and correlate correctly
+    for (int i = 0; i < pids.size(); i++) {
+        cout << "Process\t" << pids[i] << ":\tACCESSES: " << processes[i]->memory_accesses << "\tFAULTS: " << processes[i]->page_faults;
+        cout << "\tFAULT_RATE: " << processes[i]->get_fault_percent() << "\tRSS: " << processes[i]->get_rss() << endl;
+    }
+
+    cout << "\nTotal memory accesses: " << totalMemAccess << endl;
+    cout << "Total page faults: " << totalPageFaults << endl;
+    //total frames is always 512
+    cout << "Free frames remaining: " << 512 - totalUsedFrames << endl << endl;
+}
+
+/**
  * The main entry point to the simulation.
  */
 int main(int argc, char** argv) {
-    // TODO: add i/o for file argument, create a flag options struct first, then parse flags
-    //if return false, print help menu and exit
-
-    //todo, print size of all processes in bytes
-    //todo, print the firtual addresses the file contains
 
     FlagOptions flags;
 
@@ -44,6 +56,7 @@ int main(int argc, char** argv) {
     //continue to file io
 
     ///////////////////////////////////////////////////////////////////////////
+    /*
     //the following code is for deliverable one!
 
     //process_1.txt should be of size 2002
@@ -90,7 +103,7 @@ int main(int argc, char** argv) {
         //allProcesses.insert(pid, process);
         cout << "Process " << pid << " is size " << process->size() << endl;
 
-
+        processAccess.close();
     }
 
     //go through rest of file getting all memory accesses
@@ -110,8 +123,79 @@ int main(int argc, char** argv) {
         cout << "Memory access for process " << pid << " at " << address << endl;
     }
 
+    memAccess.close();
+    */
+
     ///////////////////////////////////////////////////////////////////////////
 
+    int totalMemAccess = 0;
+    int totalPageFaults = 0;
+    int totalUsedFrames = 0;
+
+    ifstream memAccess(flags.filename);
+    if (memAccess.fail()) {
+        cout << "Could not open file: " << flags.filename << endl;
+        return EXIT_FAILURE;
+    }
+    //use stringstream to get all values from input file
+    stringstream stream;
+    string line;
+    //keep track of number of processes
+    int numProcesses = 0;
+    //vector of all pids, (useful for last output)
+    vector<int> allPids;
+    //vector of all Processes, for last output
+    vector<Process*> allProcesses;
+
+    //first line is number of processes
+    getline(memAccess, line);
+    stream << line;
+    stream >> numProcesses;
+
+    //iterate through number of processes to get pid and input file,
+    //then construct process using the input file
+    for (int i = 0; i < numProcesses; i++) {
+        int pid;
+        string file;
+        stringstream loop;
+        getline(memAccess, line);
+        loop << line;
+        loop >> pid;
+        loop >> file;
+
+        //start new fileio to get all data from the input file
+        ifstream processAccess(file);
+        if (processAccess.fail()) {
+            cout << "Could not open file: " << file << endl;
+            return EXIT_FAILURE;
+        }
+        Process* process = Process::read_from_input(processAccess);
+
+        allPids.push_back(pid);
+        allProcesses.push_back(process);
+        processAccess.close();
+    }
+
+    //go through rest of file getting all memory accesses
+    int pid;
+    bitset<16> address;
+
+    //skip one line
+    getline(memAccess, line);
+
+    //might put this code in simulation::Run()
+    while(!memAccess.eof()) {
+        stringstream loop;
+        getline(memAccess, line);
+        loop << line;
+        //first entry is pid, second entry is memory access
+        loop >> pid;
+        loop >> address;
+    }
+
+    memAccess.close();
+
+    printEndResults(allPids, allProcesses, totalMemAccess, totalPageFaults, totalUsedFrames);
     
     return EXIT_SUCCESS;
 }
